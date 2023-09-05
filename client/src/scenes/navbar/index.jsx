bfb1fef771995e9ddb7ab534fc24b4e1f6f35eb8
ARYANHGD01 <aryanhgd@gmail.com>
2023-09-06 00:23:56 +0530

    Update index.jsx

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
           }}
         >
-          Sociopedia
+          SocioStupid
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
